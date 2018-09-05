100281ea542a9bf4341b5a7c6f2c25a7e66f5aca
ussupx <psviswanatha@gmail.com>
2018-09-05 11:42:14 +0530

    Update asst_module.py

--- asst_module.py
+++ asst_module.py
@@ -1,39 +1,35 @@
-# asst
 import networkx as nx
 import sys
+
+
 class DataError(Exception):
     pass
+
+
 class CyclicDependency(Exception):
     pass
 
 
 def get_data(filename):
     count=0
-    global proc
-
     with open(filename) as f:
         k=f.read().split()
     data={}
     time={}
-
     pipelines=int(k[1])
-
     for i in k[2:]:
         count+=1
         temp=i.split(",")
-
         if temp[1]!='':
             data[temp[0]]=temp[1].split("-")
         else:
             data[temp[0]]=[]
         time[temp[0]]=int(temp[2])
-
-
     if len(data)!=int(k[0]):
         print(len(data),k[0])
-        raise DataError("improper data format in the file:number of jobs referred:" +k[0]+"and actual number "+str(len(data))+"donot match")
+        raise DataError("Improper data format in the file:number of jobs referred:" +k[0]+"and actual number "+str(len(data))+"donot match")
     if count!=len(data):
-        raise Warning("duplicate data identified. considering the lastest records")
+        raise Warning("Duplicate data identified. Proceeding with the lastest records")
 
     return data,time,pipelines
 
@@ -57,15 +53,11 @@ def contraint_check(data:"dict{dict}"):
     g=nx.Graph(data)
     try:
         print ("cycle found at :",nx.find_cycle(g))
-
     except nx.exception.NetworkXNoCycle:
         return True
 
 def get_time_from_data(filename):
-    data = get_data(sys.argv[1])
-    info = get_info(*data)
-    return get_least_time(*info)
-
+    return get_least_time(*get_info(*get_data(sys.argv[1])))
 
 if __name__ == "__main__":
     print(get_time_from_data(sys.argv[1]))
